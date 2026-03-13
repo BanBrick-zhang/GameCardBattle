c82c07b48d2386072fed1d01a8a214fa38563c93
BanBrick-zhang <73102623+BanBrick-zhang@users.noreply.github.com>
2026-03-13 22:31:11 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ const PAGE_SIZE = 6;
 const currentPage = [1, 1, 1];
 let selectedItems = [];
 let searchKeyword = '';
-const API_SEARCH = 'http://localhost:10000/api/search';
+const API_SEARCH = 'https://gamecardbattle.onrender.com/api/search';
 
 function showLoading() {
     document.getElementById('loadingOverlay').style.display = 'flex';
