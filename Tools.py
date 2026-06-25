af25c8d965ccc7dac78112f7a622c7e1a37060dc
BanBrick-zhang <73102623+BanBrick-zhang@users.noreply.github.com>
2026-06-25 23:00:51 +0800

    Delete Tools.py

--- Tools.py
+++
@@ -1,6 +0,0 @@
-def to_dict(data):
-    # 定义字段名
-    keys = ['img_url', 'game_id', 'name', 'desc', 'price',  'platform']
-    # 转换为字典列表
-    dict_list = [dict(zip(keys, item)) for item in data]
-    return dict_list
